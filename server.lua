0ec02e8ae274fa5703ef1d209740b3e93253e202
Vojtíík <70014692+vjota2@users.noreply.github.com>
2021-06-20 15:09:15 +0200

    Added admin command

--- server.lua
+++ server.lua
@@ -1,3 +1,6 @@
+local adminHex = {
+    'steam:xxxx',
+}
 local afkPlayers = {}
 
 RegisterNetEvent('vojtik_afk:pleaseDontBlameMe')
@@ -26,3 +29,46 @@ RegisterCommand('afk', function(src, args)
         end
     end
 end)
+
+RegisterCommand('afkid', function(src, args)
+    local plyId = tonumber(args[1])
+    local argument = table.concat(args, " ", 2, #args)
+
+    if not isAdmin(src) then return end
+
+    if plyId == nil or GetPlayerPing(plyId) == 0 then
+        -- add notify msg, mby?
+        return 
+    end
+
+    if argument == '' then
+        if afkPlayers[plyId] then
+            afkPlayers[plyId] = nil
+            TriggerClientEvent('vojtik_afk:whyTwoEvents', plyId, false)
+            TriggerClientEvent('vojtik_afk:cringe', -1, 'remove', {sId = plyId})
+        end
+        return 
+    end
+
+    if args[1] ~= '' then
+        if argument then
+            afkPlayers[plyId] = argument
+
+            local time = os.date("*t")
+            TriggerClientEvent('vojtik_afk:whyTwoEvents', plyId, true)
+            TriggerClientEvent('vojtik_afk:cringe', -1, 'add', {sId = plyId, text = '~b~- AFK -~w~\n~b~Since:~w~ '..time.hour..':'..time.min..'\n~b~Reason:~w~ '..argument})
+        end
+    end
+end, false)
+
+function isAdmin(src)
+    local srcSteam = GetPlayerIdentifiers(src)[1]
+    local result = false
+    for k, v in pairs(adminHex) do
+        if v == srcSteam then
+            result = true
+            break
+        end
+    end
+    return result
+end
